2523196bbe0070a740efb041f7717e1345be4370
Jarvis Choy <choyja2@oocl.com>
2024-11-21 11:26:20 +0800

    feat: AC2

--- src/afs/training/oo/Truck.java
+++ src/afs/training/oo/Truck.java
@@ -1,17 +1,17 @@
 package afs.training.oo;
 
-public class Bus {
+public class Truck {
     private String name;
 
     private Integer speed;
 
-    public Bus(String name, Integer speed) {
+    public Truck(String name, Integer speed) {
         this.name = name;
         this.speed = speed;
     }
 
     public void speedUp() {
-        speed += 5;
+        speed += 2;
         System.out.printf("%s: speed up to %d km/h%n", name, speed);
     }
 }
